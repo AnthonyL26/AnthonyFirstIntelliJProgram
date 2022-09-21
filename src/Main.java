dd33e63058425c9bd6318a7684dda8139cf0c020
1E10 Laptop <sample@bths.edu>
2022-09-21 10:32:21 -0400

    2nd Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        double y = 10.54555;
+        double round = (double) Math.round(y*100)/100;
+        System.out.println(round);
+
+    }
 }
